a1fe694f850c4c8b92bad6eb1070bdc8fdb22659
Roman Shuvalov <pffbread@gmail.com>
2020-06-05 19:36:18 +0300

    update

---
+++ client/build/precache-manifest.9ac8736b55f105b554316c418f70cab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9a866daf0f951aa5ee2f0bf3ed2f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e255d69eb74b433cca3d",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "749bc17801a8ddb71434",
+    "url": "/static/css/main.1f0d7755.chunk.css"
+  },
+  {
+    "revision": "e255d69eb74b433cca3d",
+    "url": "/static/js/2.50208be1.chunk.js"
+  },
+  {
+    "revision": "b8058a7904e0d2e88fee61fc27f8dc8d",
+    "url": "/static/js/2.50208be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749bc17801a8ddb71434",
+    "url": "/static/js/main.37a30a63.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
