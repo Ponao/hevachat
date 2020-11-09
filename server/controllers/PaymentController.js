4a24bc8adb211a14efba5f8694c26d2eaa1b9c29
Roman Shuvalov <pffbread@gmail.com>
2020-11-09 17:44:35 +0400

    fix

--- server/controllers/PaymentController.js
+++ server/controllers/PaymentController.js
@@ -178,7 +178,7 @@ module.exports = {
 
                 params.paymentToken = paymentToken
                 params.preAuth = true
-                params.merchant = 'hevachat'
+                params.merchant = 'P300436250455_8069'
                 params.email = 'pffbread@gmail.com'
 
                 params.orderNumber = payment._id
@@ -270,7 +270,7 @@ module.exports = {
 
                 params.paymentToken = paymentData
                 params.preAuth = true
-                params.merchant = 'hevachat'
+                params.merchant = 'P300436250455_8069'
                 params.language = 'ru'
                 params.additionalParameters = {email: 'test@test.ru'}
                 params.description = 'ОПИСАНИЕ'
